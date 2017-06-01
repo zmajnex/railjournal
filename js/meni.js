e46c1ad288891faa0596023c3c34b6705ca44897
zmajnex <zmajnex@yahoo.com>
2017-06-01 14:49:09 +0200

    Add files via upload

---
+++ js/meni.js
@@ -0,0 +1,17 @@
+
+function myFunction() {
+    var x = document.getElementById("demo");
+    if (x.className.indexOf("w3-show") == -1) {
+        x.className += " w3-show";
+    } else { 
+        x.className = x.className.replace(" w3-show", "");
+    }
+}
+function Function2() {
+    var x = document.getElementById("search");
+    if (x.className.indexOf("w3-show") == -1) {
+        x.className += " w3-show";
+    } else { 
+        x.className = x.className.replace(" w3-show", "");
+    }
+}
